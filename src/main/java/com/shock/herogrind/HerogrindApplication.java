2c532b58f5157c3a13eb6512848bf2a3378e911e
Gseynave <gauthier.seynave@gmail.com>
2026-08-23 11:49:24 +0200

    initial commit

---
+++ src/main/java/com/shock/herogrind/HerogrindApplication.java
@@ -0,0 +1,13 @@
+package com.shock.herogrind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerogrindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerogrindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shock/herogrind/HerogrindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shock.herogrind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerogrindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
